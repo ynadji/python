f4ee7ccd12ba6db3e22fb0daed022095d50a770f
Jason Soo <wwwjscom@gmail.com>
2008-05-06 18:31:46 -0500

    Cleaned up el code

--- client.py
+++ client.py
@@ -15,19 +15,12 @@
 # USE TCP/IP or UDP...
 
 
-# File: asyncore-example-1.py
-
 import asyncore
-import socket, time
+import socket
 
-# reference time (in seconds since 1900-01-01 00:00:00)
-TIME1970 = 2208988800L # 1970-01-01 00:00:00
+class Client(asyncore.dispatcher):
 
-class TimeRequest(asyncore.dispatcher):
-    # time requestor (as defined in RFC 868)
-
-    #def __init__(self, host, port=37):
-    def __init__(self, host, port=8037):
+    def __init__(self, host, port=8038):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, port))
@@ -42,14 +35,11 @@ class TimeRequest(asyncore.dispatcher):
         self.close() # connection failed, shutdown
 
     def handle_read(self):
-        # get local time
-        here = int(time.time()) + TIME1970
 
-        # get and unpack server time
+        # get from server
         s = self.recv(4)
-        there = ord(s[3]) + (ord(s[2])<<8) + (ord(s[1])<<16) + (ord(s[0])<<24L)
-
-        self.adjust_time(int(here - there))
+	print "Received: ",s
+        delta = "balls"
 
         self.handle_close() # we don't expect more data
 
@@ -58,11 +48,10 @@ class TimeRequest(asyncore.dispatcher):
 
     def adjust_time(self, delta):
         # override this method!
-        print "time difference is", delta
+        print "k", delta
 
-#
 # try it out
 
-#request = TimeRequest("www.python.org")
+request = Client("localhost")
 
-#asyncore.loop()
+asyncore.loop()
--- server.py
+++ server.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python 
 
-# File: asyncore-example-2.py
-
 import asyncore
-import socket, time
+import socket
 import httplib
 import re
 from shared import *
 
-# reference time
-TIME1970 = 2208988800L
 
 class Channel(asyncore.dispatcher):
 
 	def handle_write(self):
-		t = int(time.time()) + TIME1970
-		t = chr(t>>24&255) + chr(t>>16&255) + chr(t>>8&255) + chr(t&255)
-		self.send(t)
+		cords = "12-4"
+		self.send(cords)
 		self.close()
 
 
@@ -28,10 +23,8 @@ class Server(asyncore.dispatcher):
 		self.port = port
 		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		#IP = sh.getIP('0') # Determine the hosts IP address
-		IP = getIP('0') # Determine the hosts IP address
+		IP = getIP('1') # Determine the hosts IP address
 		ADDR = (IP, port)
-		print "ADDR: ",ADDR
 
 		self.bind(ADDR)
 		self.listen(5)
